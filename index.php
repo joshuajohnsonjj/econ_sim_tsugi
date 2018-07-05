19917030b56437f57b97c5c8424d25b6a44f8835
Joshua Johnson <joshuajohnsonjj38@gmail.com>
2018-07-05 09:41:27 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,16 +6,17 @@ use \Tsugi\Core\LTIX;
 
 $LAUNCH = LTIX::session_start();
 var_dump($LAUNCH->user);
-// if ( !$USER->instructor ) { // if student add to user table and redirect to student UI
-// 	$result = $mysqli->query("SELECT * FROM Users WHERE email='".$USER->email."'");
-// 	if ($result->num_rows == 0) { // User does not exist in database table so add it
-// 		$add_usr_sql = "INSERT INTO Users (email) VALUES ('".$USER->email."')";
-// 		$mysqli->query($add_usr_sql);
-// 	}
-//     header("Location: src/student.php");
-// } else { // if instructor redirect to admin UI
+if ( !$LAUNCH->user->instructor ) { // if student add to user table and redirect to student UI
+	$result = $mysqli->query("SELECT * FROM Users WHERE email='".$LAUNCH->user->email."'");
+	if ($result->num_rows == 0) { // User does not exist in database table so add it
+		$add_usr_sql = "INSERT INTO Users (email) VALUES ('".$LAUNCH->user->email."')";
+		$mysqli->query($add_usr_sql);
+	}
+    header("Location: src/student.php");
+} else { // if instructor redirect to admin UI
 //     header("Location: src/admin_page.php");
-// }
+    var_dump("Instructor status: ".$LAUNCH->user->instructor);
+}
 
 ?>
 
