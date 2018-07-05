d30ab227e861794cf24a8c9156c37742b2f9bf5d
Joshua Johnson <joshuajohnsonjj38@gmail.com>
2018-07-05 10:26:09 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,16 +6,18 @@ use \Tsugi\Core\LTIX;
 
 $LAUNCH = LTIX::session_start();
 
-if ( !$LAUNCH->user->instructor ) { // if student add to user table and redirect to student UI
-	$result = $mysqli->query("SELECT * FROM Users WHERE email='".$LAUNCH->user->email."'");
-	if ($result->num_rows == 0) { // User does not exist in database table so add it
-		$add_usr_sql = "INSERT INTO Users (email) VALUES ('".$LAUNCH->user->email."')";
-		$mysqli->query($add_usr_sql);
-	}
-    header("Location: ".addSession("src/student.php"));
-} else { // if instructor redirect to admin UI
-    header("Location: ".addSession("src/admin_page.php"));
-}
-
+// if ( !$LAUNCH->user->instructor ) { // if student add to user table and redirect to student UI
+// 	$result = $mysqli->query("SELECT * FROM Users WHERE email='".$LAUNCH->user->email."'");
+// 	if ($result->num_rows == 0) { // User does not exist in database table so add it
+// 		$add_usr_sql = "INSERT INTO Users (email) VALUES ('".$LAUNCH->user->email."')";
+// 		$mysqli->query($add_usr_sql);
+// 	}
+//     header("Location: ".addSession("src/student.php"));
+// } else { // if instructor redirect to admin UI
+//     header("Location: ".addSession("src/admin_page.php"));
+// }
+jQuery.get("../../cgi-bin/printenv, function(response) { 
+	var_dump(response);
+});
 ?>
 
