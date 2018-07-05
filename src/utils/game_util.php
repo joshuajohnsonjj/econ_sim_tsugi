abade545a8c3854b7ab9b1bdea0c0b26a9ab886a
Joshua Johnson <joshuajohnsonjj38@gmail.com>
2018-07-05 13:54:05 -0400

    Update game_util.php

--- src/utils/game_util.php
+++ src/utils/game_util.php
@@ -28,7 +28,7 @@ else if (isset($_POST['mode']) && isset($_POST['difficulty']) && isset($_POST['m
 	if ($_POST['gameId']) {
 		$sql = "UPDATE Games SET name='".$_POST['gameName']."', difficulty='".$_POST['difficulty']."', mode='".$_POST['mode']."', market_struct='".$_POST['market_struct']."', macro_econ='".$_POST['macroEconomy']."', rand_events='".$rand."', time_limit='".$_POST['limit']."', num_rounds='".$_POST['numRounds']."', demand_intercept='".$_POST['demand_intercept']."', demand_slope='".$_POST['demand_slope']."', fixed_cost='".$_POST['fixed_cost']."', const_cost='".$_POST['const_cost']."' WHERE course_id=".$_POST['course_id'];
 		if ($mysqli->query($sql) === TRUE) 
-			header("Location: ../../src/admin_page.php?game=".$_POST['gameId']);
+			header("Location: ".addSession("../../src/admin_page.php?game=".$_POST['gameId']));
 		else 
 		    echo "Error: " . $sql . "<br>" . $mysqli->error;
 	}
@@ -36,7 +36,7 @@ else if (isset($_POST['mode']) && isset($_POST['difficulty']) && isset($_POST['m
 		$sql = "INSERT INTO Games (name, type, course_id, difficulty, mode, market_struct, macro_econ, rand_events, time_limit, num_rounds, demand_intercept, demand_slope, fixed_cost, const_cost)
 		VALUES ('".$_POST['gameName']."', '".$_POST['type']."', '".$_POST['course_id']."', '".$_POST['difficulty']."', '".$_POST['mode']."', '".$_POST['market_struct']."', '".$_POST['macroEconomy']."', '".$rand."', '".$_POST['limit']."', '".$_POST['numRounds']."', '".$_POST['demand_intercept']."', '".$_POST['demand_slope']."', '".$_POST['fixed_cost']."', '".$_POST['const_cost']."')";
 		if ($mysqli->query($sql) === TRUE) 
-			header("Location: ../../src/admin_page.php?course=".$_POST['course_id']);
+			header("Location: ".addSession("../../src/admin_page.php?course=".$_POST['course_id']));
 		else 
 		    echo "Error: " . $sql . "<br>" . $mysqli->error;
 	}	
@@ -47,7 +47,7 @@ else if (isset($_POST['deleteId']) && isset($_POST['deletedGameCourse'])) {
 	$delete_sql = "DELETE FROM Games WHERE id='".$_POST['deleteId']."'";
 
 	if ($mysqli->query($delete_sql) === TRUE) 
-		header("Location: ../../econ_sim/src/admin_page.php?course=".$_POST['deletedGameCourse']);
+		header("Location: ".addSession("../../econ_sim/src/admin_page.php?course=".$_POST['deletedGameCourse']));
 	else 
 	    echo "Error: " . $delete_sql . "<br>" . $mysqli->error;
 }
