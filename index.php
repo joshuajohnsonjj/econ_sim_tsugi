f6fb83fba614a83f2ddf18a9e9d8d462fea640b6
Joshua Johnson <joshuajohnsonjj38@gmail.com>
2018-07-05 09:37:53 -0400

    Update index.php

--- index.php
+++ index.php
@@ -5,17 +5,17 @@ require_once "../config.php";
 use \Tsugi\Core\LTIX;
 
 $LAUNCH = LTIX::session_start();
-
-if ( !$USER->instructor ) { // if student add to user table and redirect to student UI
-	$result = $mysqli->query("SELECT * FROM Users WHERE email='".$USER->email."'");
-	if ($result->num_rows == 0) { // User does not exist in database table so add it
-		$add_usr_sql = "INSERT INTO Users (email) VALUES ('".$USER->email."')";
-		$mysqli->query($add_usr_sql);
-	}
-    header("Location: src/student.php");
-} else { // if instructor redirect to admin UI
-    header("Location: src/admin_page.php");
-}
+var_dump($LAUNCH);
+// if ( !$USER->instructor ) { // if student add to user table and redirect to student UI
+// 	$result = $mysqli->query("SELECT * FROM Users WHERE email='".$USER->email."'");
+// 	if ($result->num_rows == 0) { // User does not exist in database table so add it
+// 		$add_usr_sql = "INSERT INTO Users (email) VALUES ('".$USER->email."')";
+// 		$mysqli->query($add_usr_sql);
+// 	}
+//     header("Location: src/student.php");
+// } else { // if instructor redirect to admin UI
+//     header("Location: src/admin_page.php");
+// }
 
 ?>
 
