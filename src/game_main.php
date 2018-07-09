2d033286b4cbd538374de22fecffa28f9dbfabb0
Joshua Johnson <joshuajohnsonjj38@gmail.com>
2018-07-09 08:48:46 -0400

    Update game_main.php

--- src/game_main.php
+++ src/game_main.php
@@ -332,7 +332,6 @@ Contains code for the game UI.
     <script src="../js/node_modules/chart.js/dist/Chart.js"></script>
 	<script src="//cdn.jsdelivr.net/npm/alertifyjs@1.11.1/build/alertify.min.js"></script>
 	<script src="http://localhost:8080/socket.io/socket.io.js"></script>
-	<script src="{% static 'designer/js/jquery.cookie.js' %}"></script>
     <script type="text/javascript">	
     	// STUFF FOR SOCKET.IO
     	// ===================
