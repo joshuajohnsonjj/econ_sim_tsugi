2b062f5d0d483a42c23456845d8355a1743c4683
Joshua Johnson <joshuajohnsonjj38@gmail.com>
2018-07-09 08:42:34 -0400

    Update game_main.php

--- src/game_main.php
+++ src/game_main.php
@@ -333,22 +333,7 @@ Contains code for the game UI.
 	<script src="//cdn.jsdelivr.net/npm/alertifyjs@1.11.1/build/alertify.min.js"></script>
 	<script src="http://localhost:8080/socket.io/socket.io.js"></script>
 	<script src="{% static 'designer/js/jquery.cookie.js' %}"></script>
-    <script type="text/javascript">
-	var csrftoken = $.cookie('csrftoken');
-
-	function csrfSafeMethod(method) {
-	    // these HTTP methods do not require CSRF protection
-	    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-	}
-
-	$.ajaxSetup({
-	    beforeSend: function(xhr, settings) {
-		if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-		    xhr.setRequestHeader("X-CSRFToken", csrftoken);
-		}
-	    }
-	});
-	    
+    <script type="text/javascript">	
     	// STUFF FOR SOCKET.IO
     	// ===================
     	// connect to server 
