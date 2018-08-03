fb5337956af912f4f6e8ac5f068a310d1ae41442
Joshua Johnson <joshuajohnsonjj38@gmail.com>
2018-08-03 11:15:32 -0400

    Update game_util.php

--- src/utils/game_util.php
+++ src/utils/game_util.php
@@ -1,35 +1,38 @@
 <?php
-/*
-add_game.php
+/* add_game.php
 
-Code for saving game, as well as deleting from mysql database.
-When saving, checks if game exists alreay to decide wheter to save as new,
-or update existing.
+- handles saving game/deleting from mysql database.
+- When saving, checks if game exists alreay to decide wheter to save as new,
+or update existing.\
+
+Last Update:
 */
-
 ini_set('display_errors', 1); error_reporting(-1);
 include 'sql_settup.php';
 require_once "../../../config.php";
-use \Tsugi\Core\LTIX;
-$LAUNCH = LTIX::session_start();
 
-// saves equilibrium to game in table
-// if (isset($_POST['equilibrium'])) {
-// 	$mysqli->query("UPDATE Games Set equilibrium='".$_POST['equilibrium']."' WHERE id=".$_POST['id']);
-// }
 // Create/save game game
 if (isset($_POST['mode']) && isset($_POST['difficulty']) && isset($_POST['market_struct']) 
-	&& isset($_POST['macroEconomy']) && isset($_POST['randEvents']) && isset($_POST['limit']) 
+	&& isset($_POST['macroEconomy']) && isset($_POST['limit']) 
 	&& isset($_POST['numRounds']) && isset($_POST['course_id']) && isset($_POST['gameName'])) {
-
-	if ($_POST['randEvents'] == "true")
-		$rand = true;
-	else
-		$rand = false;
-
+	
+	// get max value for quantity, in order to keep price/demand positive
+	switch ($_POST['market_struct']) {
+		case 'monopoly': 
+			$max=($_POST['demand_intercept']/$_POST['demand_slope'])-1;
+			break;
+		case 'oligopoly':
+			$max=(($_POST['demand_intercept']/$_POST['demand_slope'])/2)-1;
+			break;
+		case 'monopolistic':
+		case 'perfect':
+			$max=500;
+			break;
+	}
+	
 	// check if game already exists, if so, update rather than create new
 	if ($_POST['gameId']) {
-		$sql = "UPDATE Games SET name='".$_POST['gameName']."', difficulty='".$_POST['difficulty']."', equilibrium='".$_POST['equilibrium']."', mode='".$_POST['mode']."', market_struct='".$_POST['market_struct']."', macro_econ='".$_POST['macroEconomy']."', rand_events='".$rand."', time_limit='".$_POST['limit']."', num_rounds='".$_POST['numRounds']."', demand_intercept='".$_POST['demand_intercept']."', demand_slope='".$_POST['demand_slope']."', fixed_cost='".$_POST['fixed_cost']."', const_cost='".$_POST['const_cost']."' WHERE id=".$_POST['gameId'];
+		$sql = "UPDATE Games SET name='".mysqli_real_escape_string($mysqli, $_POST['gameName'])."', difficulty='".$_POST['difficulty']."', mode='".$_POST['mode']."', market_struct='".$_POST['market_struct']."', macro_econ='".$_POST['macroEconomy']."', time_limit='".$_POST['limit']."', num_rounds='".$_POST['numRounds']."', demand_intercept='".$_POST['demand_intercept']."', demand_slope='".$_POST['demand_slope']."', fixed_cost='".$_POST['fixed_cost']."', const_cost='".$_POST['const_cost']."', max_quantity='".$max."' WHERE course_id=".$_POST['course_id'];
 		if ($mysqli->query($sql) === TRUE) {
 			$url = "../../src/admin_page.php?game=".$_POST['gameId'];
 			header("Location: ".addSession($url));
@@ -38,8 +41,8 @@ if (isset($_POST['mode']) && isset($_POST['difficulty']) && isset($_POST['market
 		    echo "Error: " . $sql . "<br>" . $mysqli->error;
 	}
 	else {
-		$sql = "INSERT INTO Games (name, equilibrium, type, course_id, difficulty, mode, market_struct, macro_econ, rand_events, time_limit, num_rounds, demand_intercept, demand_slope, fixed_cost, const_cost)
-		VALUES ('".$_POST['gameName']."', '".$_POST['equilibrium']."', '".$_POST['type']."', '".$_POST['course_id']."', '".$_POST['difficulty']."', '".$_POST['mode']."', '".$_POST['market_struct']."', '".$_POST['macroEconomy']."', '".$rand."', '".$_POST['limit']."', '".$_POST['numRounds']."', '".$_POST['demand_intercept']."', '".$_POST['demand_slope']."', '".$_POST['fixed_cost']."', '".$_POST['const_cost']."')";
+		$sql = "INSERT INTO Games (name, type, course_id, difficulty, mode, market_struct, macro_econ, time_limit, num_rounds, demand_intercept, demand_slope, fixed_cost, const_cost, max_quantity)
+		VALUES ('".mysqli_real_escape_string($mysqli, $_POST['gameName'])."', '".$_POST['type']."', '".$_POST['course_id']."', '".$_POST['difficulty']."', '".$_POST['mode']."', '".$_POST['market_struct']."', '".$_POST['macroEconomy']."', '".$_POST['limit']."', '".$_POST['numRounds']."', '".$_POST['demand_intercept']."', '".$_POST['demand_slope']."', '".$_POST['fixed_cost']."', '".$_POST['const_cost']."', '".$max."')";
 		if ($mysqli->query($sql) === TRUE) {
 			$url = "../../src/admin_page.php?course=".$_POST['course_id'];
 			header("Location: ".addSession($url));
